8fd34bfc4e487bd38d7da2a90360eaec1bd7ea70
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-10-16 12:25:28 +0200

    Dvin update

--- src/Components/DvinContainerBuilder.cs
+++ src/Components/DvinContainerBuilder.cs
@@ -1,7 +1,7 @@
 ﻿using Aspenlaub.Net.GitHub.CSharp.Dvin.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.Dvin.Repositories;
-using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.PeghStandard;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Aspenlaub.Net.GitHub.CSharp.DvinStandard.Components {
